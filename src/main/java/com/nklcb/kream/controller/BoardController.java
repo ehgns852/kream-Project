cd84e6f72648f6adee38ab992a74d9533a8d898a
dohun <ehgns5668@naver.com>
2021-12-14 08:19:41 +0900

    add BoardRepository -findAll()

--- src/main/java/com/nklcb/kream/controller/BoardController.java
+++ src/main/java/com/nklcb/kream/controller/BoardController.java
@@ -1,15 +1,28 @@
 package com.nklcb.kream.controller;
 
+import com.nklcb.kream.model.Board;
+import com.nklcb.kream.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
 
+    private final BoardRepository boardRepository;
+
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+        List<Board> boards = boardRepository.findAll();
+
+        model.addAttribute("boards",boards);
+
         return "board/list";
     }
 }
