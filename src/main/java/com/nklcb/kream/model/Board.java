7c2034c8b3ae5304e18a0e019cb9e9f8e34e7d80
dohun <ehgns5668@naver.com>
2021-12-14 07:40:12 +0900

    add board Entity

--- src/main/java/com/nklcb/kream/model/Board.java
+++ src/main/java/com/nklcb/kream/model/Board.java
@@ -1,2 +1,29 @@
-package com.nklcb.kream.model;public class Board {
+package com.nklcb.kream.model;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Board {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "board_id")
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Board() {
+    }
+
+    public Board(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
