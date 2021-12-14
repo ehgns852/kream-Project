078d7ddc33257885e9031a09f10f142ff2035372
dohun <ehgns5668@naver.com>
2021-12-14 10:25:41 +0900

    add Validation

--- src/main/java/com/nklcb/kream/form/BoardForm.java
+++ src/main/java/com/nklcb/kream/form/BoardForm.java
@@ -3,12 +3,19 @@ package com.nklcb.kream.form;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 public class BoardForm {
 
+
+
     private Long id;
 
+    @NotNull
+    @Size(min = 2, max = 20)
     private String title;
 
     private String content;
