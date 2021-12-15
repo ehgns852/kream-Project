f18797c9691574701a7d1d94197f5e98b325736d
dohun <ehgns5668@naver.com>
2021-12-15 09:41:51 +0900

    Add page and size in view column QueryDSL will be changed later

--- src/main/java/com/nklcb/kream/controller/BoardController.java
+++ src/main/java/com/nklcb/kream/controller/BoardController.java
@@ -5,6 +5,10 @@ import com.nklcb.kream.form.BoardForm;
 import com.nklcb.kream.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,9 +27,12 @@ public class BoardController {
     private final BoardRepository boardRepository;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
-
+    public String list(Model model, @PageableDefault(size = 20) Pageable pageable) {
+        Page<Board> boards = boardRepository.findAll(pageable);
+        int startPage = boards.getPageable().getPageNumber() + 1;
+        int endPage = boards.getTotalPages();
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
 
         return "board/list";
