07c96beb885bb0670eb9157617a91fb1212a2afe
dohun <ehgns5668@naver.com>
2021-12-22 04:41:16 +0900

    add OAuth2UserService and update WebSecurityConfig

--- src/main/java/com/nklcb/kream/config/WebSecurityConfig.java
+++ src/main/java/com/nklcb/kream/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.nklcb.kream.config;
 
+import com.nklcb.kream.config.auth.PrincipalOauth2UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +19,11 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity //스프링 시큐리티 필터가 스프링 필터체인에 등록됨
+@RequiredArgsConstructor
 //@EnableGlobalMethodSecurity(securedEnabled = true)//secured 어노테이션 활성화 @Secured("ROLE_ADMIN")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
+    private final PrincipalOauth2UserService principalOauth2UserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,13 +39,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/account/login")
                 .loginProcessingUrl("/account/login") //login 주소가 호출이 되면 시큐리티가 낚아채 준다.
                 .defaultSuccessUrl("/")
-                .and()
-                .oauth2Login()
-                .loginPage("/account/login") //구글 로그인이 완료된 뒤의 후처리가 필요함
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
+                /**
+                 *구글 로그인이 완료된 뒤의 후처리가 필요함 1.코드받기(인증), 2.엑세스토큰(권한)
+                 *사용자 프로필 정보를 가져오고, 4.그 정보를 토대로 회원가입을 시키기도 함
+                 */
+        http
+                .oauth2Login()
+                .loginPage("/account/login")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
+
+
     }
 
 //    @Autowired
